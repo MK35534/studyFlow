0caa92f41585fe58c1d11f8416d76d6d2eff21aa
MK35534 <mderencourt@gmail.com>
2025-09-13 21:41:53 +0200

    FIx

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,4 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-  experimental: {
-    treeshakeClientOnly: false
-  }
+  modules: ['@nuxtjs/tailwindcss']
 })
