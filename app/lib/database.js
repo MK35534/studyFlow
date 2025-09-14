faded2f4927e651ff0aa9cca8304f2f6edb66e4b
MK35534 <mderencourt@gmail.com>
2025-09-14 10:04:02 +0200

    Fix db

--- app/lib/database.js
+++ app/lib/database.js
@@ -7,6 +7,7 @@ export async function getConnection() {
     try {
       connection = await mysql.createConnection({
         host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 23161,
         user: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD || '',
         database: process.env.DB_NAME || 'studyflow'
